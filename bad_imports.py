1256397f826e212790446a436ada35ef8069429a
Josh Wren <josh.wren@life.church>
2023-04-07 20:50:07 -0500

    added test file

---
+++ bad_imports.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+import scipy
+import statsmodels
+import tensorflow
+import keras
+import theano
+import nltk
+import gensim
+import spacy
+import pytorch
+import pytorch_lightning
+import pytorch_geometric
+import pytorch3d
+import pytorch3d.structures
+import pytorch3d.renderer
+import pytorch3d.renderer.cameras
+import pytorch3d.renderer.mesh
+import pytorch3d.renderer.mesh.rasterizer
+import pytorch3d.renderer.mesh.shaders
+import pytorch3d.renderer.mesh.textures
+import pytorch3d.renderer.points
+import pytorch3d.renderer.points.rasterizer
+import pytorch3d.renderer.points.shaders
+from pytorch3d.renderer import look_at_view_transform
+from pytorch3d.renderer import FoVPerspectiveCameras
+from pytorch3d.renderer import PointLights
+from pytorch3d.renderer import RasterizationSettings
+from pytorch3d.renderer import MeshRenderer
+from pytorch3d.renderer import MeshRasterizer
+from pytorch3d.renderer import SoftPhongShader
+from pytorch3d.renderer import TexturesVertex
+
+from pytorch3d.renderer import (
+    look_at_view_transform,
+    FoVPerspectiveCameras,
+    PointLights,
+    RasterizationSettings,
+    MeshRenderer,
+    MeshRasterizer,
+    SoftPhongShader,
+    TexturesVertex,
+)
+
+print("Hello World")
